373206d4156c676841c45b1938a45d35846946ba
Shelagh Lewins <shelagh_lewins@hotmail.com>
2019-12-03 23:49:13 +0000

    Work on preview

--- imports/client/components/PatternSummary.js
+++ imports/client/components/PatternSummary.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Button } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -7,57 +7,85 @@ import DOMPurify from 'dompurify';
 import { removePattern } from '../modules/pattern';
 import './PatternSummary.scss';
 
-function PatternSummary({
-	name,
-	_id,
-	dispatch,
-	patternPreview,
-}) {
-	const handleClickButtonRemove = function () {
-		const response = confirm(`Do you want to delete the pattern "${name}"?`); // eslint-disable-line no-restricted-globals
+class PatternSummary extends PureComponent {
+	constructor(props) {
+		super(props);
 
-		if (response === true) {
-			dispatch(removePattern(_id));
+		this.state = {
+			'gotSVG': false,
+		};
+
+		this.myRef = React.createRef();
+	}
+
+	componentDidUpdate = () => {
+		// to center the SVG, we need to know its width
+		const { gotSVG } = this.state;
+		const outerElm = this.myRef.current; // preview bounding div
+		const innerElm = outerElm.getElementsByTagName('svg')[0]; // svg
+		if (innerElm && !gotSVG) {
+			const outerRectWidth = outerElm.getBoundingClientRect().width;
+			const innerRectWidth = innerElm.getBoundingClientRect().width;
+
+			if (outerRectWidth > innerRectWidth) {
+				innerElm.style.marginLeft = `${(outerRectWidth - innerRectWidth) / 2}px`;
+			}
+
+			this.setState({
+				'gotSVG': true,
+			});
 		}
 	};
 
-	// import the preview svg data
-	let clean = '';
-	if (patternPreview) {
-		clean = DOMPurify.sanitize(patternPreview.data);
+	render() {
+		const {
+			name,
+			_id,
+			dispatch,
+			patternPreview,
+		} = this.props;
+
+		const handleClickButtonRemove = function () {
+			const response = confirm(`Do you want to delete the pattern "${name}"?`); // eslint-disable-line no-restricted-globals
+
+			if (response === true) {
+				dispatch(removePattern(_id));
+			}
+		};
+
+		// import the preview svg data
+		let clean = '';
+		if (patternPreview) {
+			clean = DOMPurify.sanitize(patternPreview.data);
+		}
+
+		const patternPreviewHolder = <div ref={this.myRef} className="pattern-preview" dangerouslySetInnerHTML={{ '__html': clean }} />;
+
+		const buttonRemove = (
+			<Button
+				type="button"
+				color="danger"
+				onClick={() => handleClickButtonRemove(_id)}
+			>
+			X
+			</Button>
+		);
+
+		return (
+			<div className="pattern-summary">
+				<div className="main">
+					<Link to={`/pattern/${_id}`}>
+						<h3>{name}</h3>
+						{patternPreviewHolder}
+					</Link>
+
+				</div>
+				<div className="footer">
+					{buttonRemove}
+				</div>
+			</div>
+		);
 	}
-	const patternPreviewElm = <div className="innertube" dangerouslySetInnerHTML={{ '__html': clean }} />;
-
-	const patternPreviewHolder = (
-		<div className="pattern-preview">
-			{patternPreviewElm}
-		</div>
-	);
-
-	const buttonRemove = (
-		<Button
-			type="button"
-			color="danger"
-			onClick={() => handleClickButtonRemove(_id)}
-		>
-		X
-		</Button>
-	);
-
-	return (
-		<div className="pattern-summary">
-			<div className="main">
-				<Link to={`/pattern/${_id}`}>
-					<h3>{name}</h3>
-					{patternPreviewHolder}
-				</Link>
-
-			</div>
-			<div className="footer">
-				{buttonRemove}
-			</div>
-		</div>
-	);
 }
 
 PatternSummary.propTypes = {
