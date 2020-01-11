e26e4d02f57cb3b408a286df807af71535e3a5ad
Shelagh Lewins <shelagh_lewins@hotmail.com>
2020-01-11 08:12:29 +0000

    made preview unit heights whole numbers, which seems to have fixed the cumulative error in calculating preview size!

--- imports/client/components/PatternPreview.js
+++ imports/client/components/PatternPreview.js
@@ -75,8 +75,10 @@ class PatternPreview extends Component {
 		}
 
 		// pick graphic size in the SVG
-		const unitWidth = 41.560534;
-		const unitHeight = 113.08752;
+		const unitWidth = 42;
+		const unitHeight = 113;
+		//const unitWidth = 41.560534;
+		//const unitHeight = 113.08752;
 
 		// screen pixel size of pick graphic, used to calculate final size of preview holder
 		const cellHeight = 54;
