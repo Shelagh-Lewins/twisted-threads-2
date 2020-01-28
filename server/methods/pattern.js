220f2d8a7915487b5b84615acb78451a8d8f1352
Shelagh Lewins <shelagh_lewins@hotmail.com>
2020-01-28 11:50:47 +0000

    work on all together row removal

--- server/methods/pattern.js
+++ server/methods/pattern.js
@@ -512,12 +512,15 @@ Meteor.methods({
 						// an element cannot be removed from an array by index
 						// so first we mark each row to remove
 						// then use 'pull'
-						for (let i = 0; i < removeNRows; i += 1) {
-							const rowIndex = i + removeRowsAt - 1;
+						const start = removeRowsAt - removeNRows + 1;
 
+						console.log('removeRowsAt', removeRowsAt);
+						for (let i = 0; i < removeNRows; i += 1) {
+							const rowIndex = i + start;
+console.log('rowIndex', rowIndex);
 							Patterns.update({ _id }, { '$set': { [`patternDesign.weavingInstructions.${rowIndex}`]: 'toBeRemoved' } });
 						}
-
+console.log('after update1', Patterns.findOne({_id}).patternDesign.weavingInstructions);
 						// NOTE if the pull fails, the numberOfRows will then be incorrect
 						// however if the following updates don't take place atomically, the client will likely show errors
 						const update2 = {};
