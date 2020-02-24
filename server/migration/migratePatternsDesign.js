284b6f9a77ab382c87c3542c79bb3a564dd7e883
Shelagh Lewins <shelagh_lewins@hotmail.com>
2020-02-24 19:40:59 +0000

    work on migrations

--- server/migration/migratePatternsDesign.js
+++ server/migration/migratePatternsDesign.js
@@ -11,9 +11,58 @@ import {
 	DEFAULT_PALETTE,
 } from '../../imports/modules/parameters';
 
-const simStyes = JSON.parse('[{\"background_color\":\"#cc0000\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":1},{\"background_color\":\"#ffd966\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":2},{\"background_color\":\"#1c4587\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":3},{\"background_color\":\"#6aa84f\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":4},{\"background_color\":\"#674ea7\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":5,\"special\":false},{\"background_color\":\"#783f04\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":6,\"special\":false},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffffff\",\"warp\":\"none\",\"style\":7},{\"background_color\":\"#ffffff\",\"line_color\":\"#cc0000\",\"warp\":\"forward\",\"style\":8},{\"background_color\":\"#ffffff\",\"line_color\":\"#cc0000\",\"warp\":\"backward\",\"style\":9},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#cc0000\",\"warp\":\"forward\",\"style\":10,\"special\":false},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#cc0000\",\"warp\":\"backward\",\"style\":11,\"special\":false},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffd966\",\"warp\":\"forward\",\"style\":12,\"special\":false},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffd966\",\"warp\":\"backward\",\"style\":14,\"special\":false},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffd966\",\"warp\":\"forward\",\"style\":14},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffd966\",\"warp\":\"backward\",\"style\":15},{\"background_color\":\"#ffffff\",\"line_color\":\"#1c4587\",\"warp\":\"forward\",\"style\":16},{\"background_color\":\"#ffffff\",\"line_color\":\"#1c4587\",\"warp\":\"backward\",\"style\":17},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#1c4587\",\"warp\":\"forward\",\"style\":18},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#1c4587\",\"warp\":\"backward\",\"style\":19},{\"background_color\":\"#ffffff\",\"line_color\":\"#6aa84f\",\"warp\":\"forward\",\"style\":20},{\"background_color\":\"#ffffff\",\"line_color\":\"#6aa84f\",\"warp\":\"backward\",\"style\":21},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#6aa84f\",\"warp\":\"forward\",\"style\":22},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#6aa84f\",\"warp\":\"backward\",\"style\":23},{\"background_color\":\"#ffffff\",\"line_color\":\"#674ea7\",\"warp\":\"forward\",\"style\":24},{\"background_color\":\"#ffffff\",\"line_color\":\"#674ea7\",\"warp\":\"backward\",\"style\":25},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#674ea7\",\"warp\":\"forward\",\"style\":26},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#674ea7\",\"warp\":\"backward\",\"style\":27},{\"background_color\":\"#ffffff\",\"line_color\":\"#783f04\",\"warp\":\"forward\",\"style\":28},{\"background_color\":\"#ffffff\",\"line_color\":\"#783f04\",\"warp\":\"backward\",\"style\":29},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#783f04\",\"warp\":\"forward\",\"style\":30},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#783f04\",\"warp\":\"backward\",\"style\":31},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffffff\",\"warp\":\"forward\",\"style\":32},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffffff\",\"warp\":\"backward\",\"style\":33},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffffff\",\"warp\":\"forward\",\"style\":34},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffffff\",\"warp\":\"backward\",\"style\":35},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffffff\",\"warp\":\"forward_empty\",\"style\":36},{\"background_color\":\"#ffffff\",\"line_color\":\"#ffffff\",\"warp\":\"backward_empty\",\"style\":37},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffffff\",\"warp\":\"forward_empty\",\"style\":40},{\"background_color\":\"#bbbbbb\",\"line_color\":\"#ffffff\",\"warp\":\"backward_empty\",\"style\":41}]');
-
 const simSpecialStyles = JSON.parse('[{\"background_color\":\"#FFFFFF\",\"name\":\"forward_2\",\"warp\":\"forward\",\"image\":\"/images/special_forward_2.svg\",\"style\":\"S1\"},{\"background_color\":\"#FFFFFF\",\"name\":\"backward_2\",\"warp\":\"backward\",\"image\":\"/images/special_backward_2.svg\",\"style\":\"S2\"},{\"background_color\":\"#FFFFFF\",\"name\":\"forward_3\",\"warp\":\"forward\",\"image\":\"/images/special_forward_3.svg\",\"style\":\"S3\"},{\"background_color\":\"#FFFFFF\",\"name\":\"backward_3\",\"warp\":\"backward\",\"image\":\"/images/special_backward_3.svg\",\"style\":\"S4\"},{\"background_color\":\"#FFFFFF\",\"name\":\"forward_4\",\"warp\":\"forward\",\"image\":\"/images/special_forward_4.svg\",\"style\":\"S5\"},{\"background_color\":\"#FFFFFF\",\"name\":\"backward_4\",\"warp\":\"backward\",\"image\":\"/images/special_backward_4.svg\",\"style\":\"S6\"},{\"background_color\":\"#FFFFFF\",\"image\":\"/images/special_empty.svg\",\"style\":\"S7\"},{\"background_color\":\"#FFFFFF\",\"image\":\"\",\"style\":\"S8\"},{\"background_color\":\"#BBBBBB\",\"name\":\"backward_2_gray\",\"warp\":\"backward\",\"image\":\"/images/special_backward_2.svg\",\"style\":\"S9\"},{\"background_color\":\"#BBBBBB\",\"name\":\"forward_2_gray\",\"warp\":\"forward\",\"image\":\"/images/special_forward_2.svg\",\"style\":\"S10\"},{\"background_color\":\"#BBBBBB\",\"name\":\"backward_3_gray\",\"warp\":\"backward\",\"image\":\"/images/special_backward_3.svg\",\"style\":\"S11\"},{\"background_color\":\"#BBBBBB\",\"name\":\"forward_3_gray\",\"warp\":\"forward\",\"image\":\"/images/special_forward_3.svg\",\"style\":\"S12\"},{\"background_color\":\"#BBBBBB\",\"name\":\"backward_4_gray\",\"warp\":\"backward\",\"image\":\"/images/special_backward_4.svg\",\"style\":\"S13\"},{\"background_color\":\"#BBBBBB\",\"name\":\"forward_4_gray\",\"warp\":\"forward\",\"image\":\"/images/special_forward_4.svg\",\"style\":\"S14\"},{\"background_color\":\"#FFFFFF\",\"name\":\"idle\",\"image\":\"/images/special_idle.svg\",\"style\":\"S15\"},{\"background_color\":\"#FFFFFF\",\"image\":\"\",\"style\":\"S16\"}]');
+
+const buildThreading = ({
+	holes,
+	orientation,
+	styles,
+	threading,
+	weft_color,
+}) => {
+	const oldStyles = JSON.parse(styles);
+	const oldOrientation = JSON.parse(orientation);
+	const oldThreading = JSON.parse(threading);
+
+	let newNumberOfRows;
+	const newThreading = new Array(holes);
+	const newPalette = DEFAULT_PALETTE;
+
+	const newNumberOfTablets = oldThreading[0].length;
+	// simulation patterns only have 7 thread colours
+	for (let i = 0; i < 7; i += 1) {
+		newPalette[i] = oldStyles[i].background_color;
+	}
+
+	newPalette[8] = weft_color; // weft color is now saved as an indexed color in the palette. 8 is the first unused palette slot.
+
+	// threading
+	for (let i = 0; i < holes; i += 1) {
+		const rowIndex = holes - i - 1; // new threading runs the other way
+		newThreading[rowIndex] = new Array(newNumberOfTablets);
+
+		for (let j = 0; j < newNumberOfTablets; j += 1) {
+			const oldStyle = oldThreading[i][j];
+
+			if (oldStyle === 'S7') {
+				newThreading[rowIndex][j] = -1; // empty hole
+			} else {
+				newThreading[rowIndex][j] = oldStyle - 1; // old styles start at 1
+			}
+		}
+	}
+
+	// orientations
+	const newOrientations = oldOrientation.map((entry) => ((entry === 'S') ? '/' : '\\'));
+
+	return {
+		newNumberOfRows,
+		newNumberOfTablets,
+		newOrientations,
+		newPalette,
+		newThreading,
+	};
+};
 
 const migratePatternsDesign = () => {
 	console.log('*** starting to migrate pattern design');
@@ -73,14 +122,15 @@ const migratePatternsDesign = () => {
 		} else {
 			// console.log('pattern does not have patternType, so migrate it: ', _id);
 			const newPatternDesign = {};
-			const newPalette = DEFAULT_PALETTE;
+			// const newPalette = DEFAULT_PALETTE;
 
 			// number of rows, tablets have been read from old data
 			// but let's make sure they are correct
 			let newNumberOfRows;
 			let newNumberOfTablets;
 			let newOrientations;
-			const newThreading = new Array(holes);
+			let newPalette;
+			let newThreading;
 			let newPatternType;
 
 			// palette
@@ -104,41 +154,24 @@ const migratePatternsDesign = () => {
 				if (missingData) {
 					Patterns.remove({ _id });
 				} else {
-					processedPatterns.push(_id);
-					const oldStyles = JSON.parse(styles);
-					const oldOrientation = JSON.parse(orientation);
-					const oldThreading = JSON.parse(threading);
-
-					newNumberOfTablets = oldThreading[0].length;
-					// simulation patterns only have 7 thread colours
-					for (let i = 0; i < 7; i += 1) {
-						newPalette[i] = oldStyles[i].background_color;
-					}
-
-					newPalette[8] = weft_color; // weft color is now saved as an indexed color in the palette. 8 is the first unused palette slot.
-
-					// threading
-					for (let i = 0; i < holes; i += 1) {
-						const rowIndex = holes - i - 1; // new threading runs the other way
-						newThreading[rowIndex] = new Array(newNumberOfTablets);
-
-						for (let j = 0; j < newNumberOfTablets; j += 1) {
-							const oldStyle = oldThreading[i][j];
-
-							if (oldStyle === 'S7') {
-								newThreading[rowIndex][j] = -1; // empty hole
-							} else {
-								newThreading[rowIndex][j] = oldStyle - 1; // old styles start at 1
-							}
-						}
-					}
-
-					// orientations
-					newOrientations = oldOrientation.map((entry) => ((entry === 'S') ? '/' : '\\'));
+					({
+						newNumberOfRows,
+						newNumberOfTablets,
+						newOrientations,
+						newPalette,
+						newThreading,
+					} = buildThreading({
+						holes,
+						orientation,
+						styles,
+						threading,
+						weft_color,
+					}));
 
 					// patternDesign
 					if (simulation_mode === 'auto') {
 						newPatternType = 'allTogether';
+						processedPatterns.push(_id);
 						newPatternDesign.weavingInstructions = auto_turn_sequence;
 						newNumberOfRows = auto_turn_sequence.length; // just in case of error in old data
 					} else if (simulation_mode === 'manual') {
@@ -151,9 +184,9 @@ const migratePatternsDesign = () => {
 							manualWithNoRows.push(_id);
 						} else {
 							newPatternType = 'individual';
+							processedPatterns.push(_id);
 							const weavingInstructions = []; // these will be built by row from manual_weaving_turns
 
-							newNumberOfTablets = oldTurns[0].tablets.length;
 							// manual patterns have a dummy row at start for working
 							for (let i = 0; i < newNumberOfRows; i += 1) {
 								weavingInstructions[i] = [];
@@ -176,6 +209,12 @@ const migratePatternsDesign = () => {
 						console.log('simulation_mode', simulation_mode);
 					}
 				}
+			} else if (edit_mode === 'broken_twill') {
+				//console.log('broken twill');
+				//check for missing data
+				// read 2 x design charts
+				// build new chart
+				// get threading etc
 			} else if (edit_mode === 'freehand') {
 				// some freehand patterns e.g. MtJ27N9QARhPwWZCP do not have special styles
 				let oldSpecialStyles;
