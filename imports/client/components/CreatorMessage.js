2c091645e7a6fbfe7cd2b77ac3f01884f88c1b53
Shelagh Lewins <shelagh_lewins@hotmail.com>
2020-07-31 08:34:05 +0100

    admustment to greeting text

--- imports/client/components/CreatorMessage.js
+++ imports/client/components/CreatorMessage.js
@@ -4,7 +4,7 @@ function CreatorMessage() {
 	return (
 		<>
 			<img src="images/creator_profile.jpg" alt="The creator of Twisted Threads" style={{'width': '80px', 'height': '80px', 'float': 'left', 'marginRight': '8px'}} />
-			<p>Hello, and welcome. I&apos;m Shelagh, the creator of Twisted Threads. TWT is free to use, but creating and maintaining it is a lot of work, and the server costs money. If you&apos;d like to support my work on TWT, I will be very grateful for any donations! You can donate using Patreon, Ko-fi or PayPal.</p>
+			<p>Hello, and welcome. I&apos;m Shelagh, the creator of Twisted Threads. It&apos;s free to use, but creating and maintaining it is a lot of work, and the server costs money. If you&apos;d like to support my work on Twisted Threads, I will be very grateful for any donations! You can donate using Patreon, Ko-fi or PayPal.</p>
 		</>
 	);
 }
