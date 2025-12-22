9768ebc89e93dcfcdd943ddea88a21f0ff960059
shelagh-lewins <shelagh_lewins@hotmail.com>
2025-12-22 16:28:51 +0000

    work on tests

--- server/test/10_utils.test.js
+++ server/test/10_utils.test.js
@@ -78,5 +78,59 @@ describe('utils.js basic unit tests', () => {
     });
   });
 
+  describe('validPaletteIndexCheck', () => {
+    it('should pass check for valid palette indices', () => {
+      [0, 5, 15, -1].forEach((val) => {
+        expect(() => check(val, utils.validPaletteIndexCheck)).to.not.throw();
+      });
+    });
+    it('should fail check for invalid palette indices', () => {
+      [16, 100, -2].forEach((val) => {
+        expect(() => check(val, utils.validPaletteIndexCheck)).to.throw();
+      });
+    });
+  });
+
+  describe('validDirectionCheck', () => {
+    it('should pass check for allowed directions', () => {
+      ['F', 'B'].forEach((val) => {
+        expect(() => check(val, utils.validDirectionCheck)).to.not.throw();
+      });
+    });
+    it('should fail check for disallowed directions', () => {
+      ['X', '', null, undefined].forEach((val) => {
+        expect(() => check(val, utils.validDirectionCheck)).to.throw();
+      });
+    });
+  });
+
+  describe('validTwillChartCheck', () => {
+    it('should pass check for allowed twill chart names', () => {
+      ['twillPatternChart', 'twillDirectionChangeChart'].forEach((val) => {
+        expect(() => check(val, utils.validTwillChartCheck)).to.not.throw();
+      });
+    });
+    it('should fail check for disallowed twill chart names', () => {
+      ['notAChart', '', null, undefined].forEach((val) => {
+        expect(() => check(val, utils.validTwillChartCheck)).to.throw();
+      });
+    });
+  });
+
+  describe('validHexColorCheck', () => {
+    it('should pass check for valid hex colors', () => {
+      ['#fff', '#aa00aa', '#123456', '#000', '#a'].forEach((val) => {
+        expect(() => check(val, utils.validHexColorCheck)).to.not.throw();
+      });
+    });
+    it('should fail check for invalid hex colors', () => {
+      ['', '#1234567', '#12345678', 'notAColor', null, undefined].forEach(
+        (val) => {
+          expect(() => check(val, utils.validHexColorCheck)).to.throw();
+        },
+      );
+    });
+  });
+
   // Add more tests for other exported functions as needed
 });
